32d9a6e6a90b47ac261751c51d4fe286f14a2f51
mdgriest <mdgriest@crimson.ua.edu>
2016-04-24 17:44:30 -0500

    Added scrollable credits page

--- app/src/main/java/commdgriest/httpsgithub/wimk/NounProjCreditScreen.java
+++ app/src/main/java/commdgriest/httpsgithub/wimk/NounProjCreditScreen.java
@@ -2,6 +2,7 @@ package commdgriest.httpsgithub.wimk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by Alexandria on 03/21/2016
